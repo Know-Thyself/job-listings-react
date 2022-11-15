71cb799e9b5a87cf6a692f294c4f7529c6462d82
Biruk Kebede <biruk.key@gmail.com>
2022-11-15 07:11:32 +0000

    implemented desktop layout

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+		<header className='App-header'>
+			<img
+				className='header-bg'
+				src='/images/bg-header-desktop.svg'
+				alt='header background'
+			/>
+		</header>
+	);
+}
+
+export default Header;
---
+++ src/components/JobListing.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import jobs from '../data.json';
+const JobListing = () => {
+  const [jobList, setJobList] = useState(jobs);
+  console.log(jobList)
+  return(
+    <main className='main'>
+      {jobList.map(job => {
+        return (
+					<div className='job-list-wrapper'>
+						<div className='left'>
+							<img src={job.logo} alt='logo' />
+							<div className='description'>
+								<div className='company-new-featured'>
+									<h4 className='company'>{job.company}</h4>
+									{job.new && <p className='new'>New!</p>}
+									{job.featured && <p className='featured'>Featured!</p>}
+								</div>
+								<h3 className='position'>{job.position}</h3>
+								<div className='details'>
+									<p>{job.postedAt}</p>
+									<p>{job.contract}</p>
+									<p>{job.location}</p>
+								</div>
+							</div>
+						</div>
+						<div className='right'>
+							<p className='role'>{job.role}</p>
+							<p className='level'>{job.level}</p>
+							{job.languages.map((language) => (
+								<p className='language'>{language}</p>
+							))}
+						</div>
+					</div>
+				);
+      })}
+    </main>
+  )
+}
+
+export default JobListing;
